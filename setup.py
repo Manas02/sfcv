fcc0456de0f3e99783595d3f658f9e27ed8cebe4
Manas Mahale <manas.mahale2002@gmail.com>
2025-06-19 12:30:19 +0530

    readme added to pypi

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 import os
 from setuptools import setup, find_packages
+from pathlib import Path
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 req_path = os.path.join(os.path.dirname(__file__), "requirements.txt")
 
@@ -8,7 +12,7 @@ with open(req_path, encoding="utf-8") as f:
 
 setup(
     name="sfcv",
-    version="0.1.0",
+    version="0.1.1",
     description="Step Forward Cross Validation for Bioactivity Prediction",
     author="Manas Mahale",
     author_email="manas.m.mahale@gmail.com",
@@ -17,4 +21,6 @@ setup(
     package_data={"sfcv": ["logdmodel.txt"]},
     install_requires=install_requires,
     python_requires=">=3.11",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
